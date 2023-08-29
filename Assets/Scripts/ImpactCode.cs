3b7c3000ae7e15d5a6cb627edb80310e89dd107c
Leonardo <almeidaleo.dev@gmail.com>
2023-08-29 14:43:42 -0300

    Criação de objetos quebráveis

---
+++ Assets/Scripts/ImpactCode.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactCode : MonoBehaviour
+{
+    private int limite;
+    private SpriteRenderer spriteR;
+    [SerializeField]
+    private Sprite[] sprites;
+    [SerializeField]
+    private GameObject bomb;
+
+    void Start()
+    {
+        limite = 0;
+        spriteR = GetComponent<SpriteRenderer>();
+        spriteR.sprite = sprites[0];  //Primeira imagem do spriteR
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //relativeVelocity = velocidade linear relativa de dois objetos colidindo
+        if(collision.relativeVelocity.magnitude > 4 && collision.relativeVelocity.magnitude < 10) //Verificar força de colisão
+        {
+            if(limite < sprites.Length - 1) //Se limite é menor do que o indice máximo permitio no array "sprites"
+            {
+                limite++; //limite é incrementado
+                spriteR.sprite = sprites[limite]; //o sprite atual é trocado pelo próximo do array
+            }
+            else if(limite ==  sprites.Length - 1)
+            {
+                Instantiate(bomb, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+        else if (collision.relativeVelocity.magnitude > 12 && collision.gameObject.CompareTag("Player"))
+        {
+            Instantiate(bomb, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+            Destroy(gameObject) ;
+        }
+    }
+}
