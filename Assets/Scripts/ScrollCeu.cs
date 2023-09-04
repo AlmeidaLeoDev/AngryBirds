a1048b7155be4fa71acde36a567bedc5037ca5fa
Leonardo <almeidaleo.dev@gmail.com>
2023-09-04 15:53:22 -0300

    UI Cena game

---
+++ Assets/Scripts/ScrollCeu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollCeu : MonoBehaviour
+{
+    public RawImage back;
+
+    void Start()
+    {
+        back = GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        back.uvRect = new Rect(0.01f * Time.time, 0, 1, 1);
+    }
+}
